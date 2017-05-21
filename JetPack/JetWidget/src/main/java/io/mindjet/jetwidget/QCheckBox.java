a929e24b7b3166517c74f5019de8dbf2fa8fda4d
Mindjet <pearl920@outlook.com>
2017-05-21 13:16:50 +0800

    Update: optimize the animation of QCheckBox

--- JetPack/JetWidget/src/main/java/io/mindjet/jetwidget/QCheckBox.java
+++ JetPack/JetWidget/src/main/java/io/mindjet/jetwidget/QCheckBox.java
@@ -6,8 +6,7 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
+import android.graphics.Point;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -28,7 +27,7 @@ public class QCheckBox extends View implements Checkable {
 
     private final int DEFAULT_BORDER_WIDTH = 2;              //in dp
     private final int DEFAULT_CHECK_WIDTH = 2;              //in dp
-    private final int DEFAULT_BORDER_COLOR = Color.BLACK;
+    private final int DEFAULT_BORDER_COLOR = Color.DKGRAY;
     private final int DEFAULT_CONTENT_COLOR = Color.RED;
     private final int DEFAULT_CHECK_COLOR = Color.WHITE;
     private final int DEFAULT_ANIMATION_DURATION = 300;
@@ -41,9 +40,11 @@ public class QCheckBox extends View implements Checkable {
     private Paint mBorderPaint;
     private Paint mContentPaint;
     private Paint mCheckPaint;
-    private Paint mBgPaint;
+    private Paint mUncheckedBgPaint;
     private Paint mContentEraser;
-    private Paint mCheckEraser;
+
+    private Point[] mCheckPoints = new Point[3];
+    private float mLeftCheckLength, mRightCheckLength, mDrawnLength;
 
     private int mWidth;
     private int mHeight;
@@ -51,6 +52,7 @@ public class QCheckBox extends View implements Checkable {
     private int mCheckWidth;        //in pixel
 
     private float mProgress;
+    private float mCheckProgress;
 
     private Canvas mCanvas;
 
@@ -81,6 +83,7 @@ public class QCheckBox extends View implements Checkable {
         mCheckWidth = typedArray.getDimensionPixelOffset(R.styleable.QCheckBox_checkWidth, dp2px(DEFAULT_CHECK_WIDTH));
         mChecked = typedArray.getBoolean(R.styleable.QCheckBox_checked, false);
         mProgress = mChecked ? 1 : 0;
+        mCheckProgress = mChecked ? 1 : 0;
         typedArray.recycle();
     }
 
@@ -88,27 +91,22 @@ public class QCheckBox extends View implements Checkable {
         mBorderPaint = makePaint(mBorderColor);
         mBorderPaint.setStyle(Paint.Style.STROKE);
         mBorderPaint.setStrokeWidth(mBorderWidth);
+
         mContentPaint = makePaint(mContentColor);
+
         mCheckPaint = makePaint(mCheckColor);
         mCheckPaint.setStrokeWidth(mCheckWidth);
         mCheckPaint.setStrokeCap(Paint.Cap.ROUND);
-        mBgPaint = makePaint(Color.LTGRAY);
 
-        mContentEraser = makePaint(Color.WHITE);
-//        mCheckEraser = makeEraser();
+        mUncheckedBgPaint = makePaint(mBorderColor);     //paint to draw background when from checked to unchecked.
+
+        mContentEraser = makePaint(Color.WHITE);    //paint to erase background.
     }
 
     private Paint makePaint(int color) {
         Paint paint = new Paint();
         paint.setColor(color);
         paint.setAntiAlias(true);
-        return paint;
-    }
-
-    private Paint makeEraser(int color) {
-        Paint paint = new Paint();
-        paint.setColor(color);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
         return paint;
     }
 
@@ -127,7 +125,16 @@ public class QCheckBox extends View implements Checkable {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         resolveMeasureSpec(widthMeasureSpec, heightMeasureSpec);
         setMeasuredDimension(mWidth, mHeight);
+        initCheckPoints();
 //        printBasicInfo();
+    }
+
+    private void initCheckPoints() {
+        mCheckPoints[0] = new Point(Math.round(mWidth * 2 / 9), Math.round(mHeight / 2));
+        mCheckPoints[1] = new Point(Math.round(mWidth * 2 / 5), Math.round(mHeight * 17 / 24));
+        mCheckPoints[2] = new Point(Math.round(mWidth * 41 / 54), Math.round(mHeight * 31 / 96));
+        mLeftCheckLength = (float) Math.sqrt(Math.pow((mCheckPoints[1].x - mCheckPoints[0].x), 2) + Math.pow((mCheckPoints[1].y - mCheckPoints[0].y), 2));
+        mRightCheckLength = (float) Math.sqrt(Math.pow((mCheckPoints[2].x - mCheckPoints[1].x), 2) + Math.pow((mCheckPoints[2].y - mCheckPoints[1].y), 2));
     }
 
     @Override
@@ -139,18 +146,26 @@ public class QCheckBox extends View implements Checkable {
     private void drawCheckBox() {
         if (mChecked) {
             mCanvas.drawCircle(mWidth / 2, mHeight / 2, mWidth / 2 * mProgress, mContentPaint);
-            float pStartX = mWidth * 2 / 9;
-            float pStartY = mHeight / 2;
-            float pTurnX = mWidth * 2 / 5;
-            float pTurnY = mHeight * 17 / 24;
-            float pEndX = mWidth * 41 / 54;
-            float pEndY = mHeight * 31 / 96;
-            mCanvas.drawLine(pStartX, pStartY, pTurnX, pTurnY, mCheckPaint);
-            mCanvas.drawLine(pTurnX, pTurnY, pEndX, pEndY, mCheckPaint);
+            drawCheck();
         } else {
-            mCanvas.drawCircle(mWidth / 2, mHeight / 2, (mWidth - mBorderWidth) / 2, mBgPaint);
+            mCanvas.drawCircle(mWidth / 2, mHeight / 2, (mWidth - mBorderWidth) / 2, mUncheckedBgPaint);
             mCanvas.drawCircle(mWidth / 2, mHeight / 2, (mWidth - mBorderWidth) / 2 * (1 - mProgress), mContentEraser);
-            mCanvas.drawCircle(mWidth / 2, mHeight / 2, (mWidth - mBorderWidth) / 2, mBorderPaint);        //the third parameter control the size of the circle.
+            mCanvas.drawCircle(mWidth / 2, mHeight / 2, (mWidth - mBorderWidth) / 2 * (1 - mProgress), mBorderPaint);        //the third parameter control the size of the circle.
+        }
+    }
+
+    private void drawCheck() {
+        mDrawnLength = mCheckProgress * (mLeftCheckLength + mRightCheckLength);
+        float targetX, targetY;
+        if (mDrawnLength < mLeftCheckLength) {
+            targetX = mCheckPoints[0].x + (mCheckPoints[1].x - mCheckPoints[0].x) * mDrawnLength / mLeftCheckLength;
+            targetY = mCheckPoints[0].y + (mCheckPoints[1].y - mCheckPoints[0].y) * mDrawnLength / mLeftCheckLength;
+            mCanvas.drawLine(mCheckPoints[0].x, mCheckPoints[0].y, targetX, targetY, mCheckPaint);
+        } else {
+            mCanvas.drawLine(mCheckPoints[0].x, mCheckPoints[0].y, mCheckPoints[1].x, mCheckPoints[1].y, mCheckPaint);
+            targetX = mCheckPoints[1].x + (mCheckPoints[2].x - mCheckPoints[1].x) * (mDrawnLength - mLeftCheckLength) / mRightCheckLength;
+            targetY = mCheckPoints[1].y + (mCheckPoints[2].y - mCheckPoints[1].y) * (mDrawnLength - mLeftCheckLength) / mRightCheckLength;
+            mCanvas.drawLine(mCheckPoints[1].x, mCheckPoints[1].y, targetX, targetY, mCheckPaint);
         }
     }
 
@@ -166,7 +181,7 @@ public class QCheckBox extends View implements Checkable {
     }
 
     private void startCheckedAnim() {
-        ValueAnimator animator = ValueAnimator.ofFloat(0.5f, 0.3f, 1f);
+        ValueAnimator animator = ValueAnimator.ofFloat(0.7f, 0.5f, 1f);
         animator.setDuration(DEFAULT_ANIMATION_DURATION);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
@@ -176,10 +191,22 @@ public class QCheckBox extends View implements Checkable {
             }
         });
         animator.start();
+        mCheckProgress = 0;
+        ValueAnimator checkAnimator = ValueAnimator.ofFloat(0f, 1f);
+        checkAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
+        checkAnimator.setStartDelay(DEFAULT_ANIMATION_DURATION);
+        checkAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mCheckProgress = (float) valueAnimator.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        checkAnimator.start();
     }
 
     private void startUncheckedAnim() {
-        ValueAnimator animator = ValueAnimator.ofFloat(0.7f, 0.5f, 1f, 0f);
+        ValueAnimator animator = ValueAnimator.ofFloat(0.5f, 0.7f, 0f);
         animator.setDuration(DEFAULT_ANIMATION_DURATION);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
@@ -233,13 +260,13 @@ public class QCheckBox extends View implements Checkable {
     }
 
     @Override
-    public void setChecked(boolean checked) {
-        mChecked = checked;
+    public boolean isChecked() {
+        return mChecked;
     }
 
     @Override
-    public boolean isChecked() {
-        return mChecked;
+    public void setChecked(boolean checked) {
+        mChecked = checked;
     }
 
     @Override
