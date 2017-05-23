e72f3bad8d6f89d62811717da5143b6c6487afae
Mindjet <pearl920@outlook.com>
2017-05-23 13:57:59 +0800

    Add: add bezier path in QLoadingView

--- JetPack/JetWidget/src/main/java/io/mindjet/jetwidget/QLoadingView.java
+++ JetPack/JetWidget/src/main/java/io/mindjet/jetwidget/QLoadingView.java
@@ -6,10 +6,12 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 /**
  * A cute loading view.
@@ -24,20 +26,26 @@ public class QLoadingView extends View {
     private final int DEFAULT_WIDTH = 40;
     private final int DEFAULT_HEIGHT = 40;
     private final int DEFAULT_BALL_RADIUS = 8;
+    private final int DEFAULT_DURATION = 2000;
 
     private final int DEFAULT_BALL_COLOR = Color.BLACK;
 
     private Canvas mCanvas;
     private int mWidth;
     private int mHeight;
+    private int mDuration;
+    private float mProgress;
     private float mRotateDegree;
     private float mOffset;
+    private boolean mEclipsed;
+    private float mPinchOffset;            //the offset when 2 balls start to pinch.
 
     private int mBallRadius;
-
     private int mBallColor;
-
     private Paint mBallPaint;
+    private Paint mBezierPaint;
+
+    private Path mBezierPath;
 
     public QLoadingView(Context context) {
         this(context, null);
@@ -51,12 +59,15 @@ public class QLoadingView extends View {
         super(context, attrs, defStyleAttr);
         initAttrs(context, attrs);
         initPaint();
+        initPath();
     }
 
     private void initAttrs(Context context, AttributeSet attrs) {
-        TypedArray array = context.obtainStyledAttributes(attrs, io.mindjet.jetwidget.R.styleable.QLoadingView);
-        mBallRadius = array.getDimensionPixelOffset(io.mindjet.jetwidget.R.styleable.QLoadingView_ballRadius, ViewUtil.dp2px(context, DEFAULT_BALL_RADIUS));
-        mBallColor = array.getColor(io.mindjet.jetwidget.R.styleable.QLoadingView_ballColor, DEFAULT_BALL_COLOR);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.QLoadingView);
+        mBallRadius = array.getDimensionPixelOffset(R.styleable.QLoadingView_ballRadius, ViewUtil.dp2px(context, DEFAULT_BALL_RADIUS));
+        mBallColor = array.getColor(R.styleable.QLoadingView_ballColor, DEFAULT_BALL_COLOR);
+        mEclipsed = array.getBoolean(R.styleable.QLoadingView_eclipsed, false);
+        mDuration = array.getInteger(R.styleable.QLoadingView_duration, DEFAULT_DURATION);
         array.recycle();
     }
 
@@ -64,6 +75,14 @@ public class QLoadingView extends View {
         mBallPaint = new Paint();
         mBallPaint.setColor(mBallColor);
         mBallPaint.setAntiAlias(true);
+        mBezierPaint = new Paint();
+        mBezierPaint.setColor(mBallColor);
+        mBezierPaint.setAntiAlias(true);
+        mBezierPaint.setStyle(Paint.Style.FILL);
+    }
+
+    private void initPath() {
+        mBezierPath = new Path();
     }
 
     @Override
@@ -74,6 +93,7 @@ public class QLoadingView extends View {
         mHeight = mWidth;
         setMeasuredDimension(mWidth, mHeight);
         mBallRadius = mBallRadius >= mWidth / 6 ? mWidth / 6 : mBallRadius;     //constraint the radius of balls.
+        mPinchOffset = mHeight / 2 - mBallRadius * 2;
         startDraw();
     }
 
@@ -85,15 +105,15 @@ public class QLoadingView extends View {
 
     private void startDraw() {
         ValueAnimator animator = ValueAnimator.ofFloat(0f, 1f);
-        animator.setDuration(2000);
-        animator.setInterpolator(new FastOutSlowInInterpolator());
+        animator.setDuration(mDuration);
+        animator.setInterpolator(mEclipsed ? new LinearInterpolator() : new FastOutSlowInInterpolator());
         animator.setRepeatCount(-1);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                float value = (float) animation.getAnimatedValue();
-                mOffset = (mHeight / 2 - mBallRadius) * Math.abs(value - 0.5f) * 2;
-                mRotateDegree = value * 360;
+                mProgress = (float) animation.getAnimatedValue();
+                mOffset = (mHeight / 2 - mBallRadius) * Math.abs(mProgress - 0.5f) * 2;
+                mRotateDegree = mProgress * 360;
                 postInvalidate();
             }
         });
@@ -102,8 +122,26 @@ public class QLoadingView extends View {
 
     private void drawLoadingView() {
         mCanvas.rotate(mRotateDegree, mWidth / 2, mHeight / 2);
-        mCanvas.drawCircle(mWidth / 2, mBallRadius + mOffset, mBallRadius, mBallPaint);
-        mCanvas.drawCircle(mWidth / 2, mHeight - mBallRadius - mOffset, mBallRadius, mBallPaint);
+        mCanvas.drawCircle(mWidth / 2, mBallRadius + (mEclipsed ? mOffset : 0), mBallRadius, mBallPaint);
+        mCanvas.drawCircle(mWidth / 2, mHeight - mBallRadius - (mEclipsed ? mOffset : 0), mBallRadius, mBallPaint);
+        //if eclipse is enabled and 2 balls start to pinch, draw bezier path.
+        if (mEclipsed && mOffset >= mPinchOffset - mBallRadius / 2) drawBezierPath();
+    }
+
+    private void drawBezierPath() {
+        mBezierPath.reset();
+        float controlOffset = mOffset - mPinchOffset;
+        float leftControlX = mWidth / 2 - controlOffset;        //from mWidth/2 to mWidth/2-mBallRadius.
+        float rightControlX = mWidth / 2 + controlOffset;       //from mWidth/2 to mWidth/2+mBallRadius.
+
+        //draw the path clockwise.
+        mBezierPath.moveTo(mWidth / 2 - mBallRadius, mBallRadius + mOffset);
+        mBezierPath.lineTo(mWidth / 2 + mBallRadius, mBallRadius + mOffset);
+        mBezierPath.quadTo(rightControlX, mHeight / 2, mWidth / 2 + mBallRadius, mHeight - mBallRadius - mOffset);
+        mBezierPath.lineTo(mWidth / 2 - mBallRadius, mHeight - mBallRadius - mOffset);
+        mBezierPath.quadTo(leftControlX, mHeight / 2, mWidth / 2 - mBallRadius, mBallRadius + mOffset);
+        mBezierPath.close();
+        mCanvas.drawPath(mBezierPath, mBezierPaint);
     }
 
 }
